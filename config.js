f40a1d389f7b2681c9ff8e01b29fd3c7d3930ac9
unknown <your-email@example.com>
2023-02-26 03:28:48 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Hôm nay là sinh nhật của em đó Thư à!',
+    introDesc: ` Thư à! Em đọc được dòng này,em phải mua thuốc đau lưng vì anh làm món quà nhỏ này cho em đó,anh dởn đấy, sinh nhật vui vẻ nha em:3 `,
+    btnIntro: 'Em bấm vào đi!',
+    title: 'Nay là ngày 01/03/2023 là ngày sinh nhật của anh, à nhầm , sinh nhật của em! 🥳',
+    desc: 'Anh rất đẹp trai và dễ thương đúng không em!,nên em sẽ bao anh đi ăn :3 ',
+    btnYes: 'Oki anh, anh là nhất <33',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Em muốn gửi lời nói gì với anh không?',
+    btnReply: 'Gửi luôn anh <3',
+    reply: 'Anh dễ thương và cute hihi <33333333',
+    mess: 'Anh biết Thư nói vậy mà , happy birthday 🎂 🎂',
+    messDesc: 'Bấm vào bên dưới đi em, đừng hồi hộp, anh cũng hồi hộp nữa hihi',
+    btnAccept: 'Bấm nè tin tin <3 ',
+    messLink: '/sinhnhatem-ne-master/happy-birthday-master/index.html' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
